2b4eb49f405d621fd1446e9c3d981122f9f3db8b
xongjaemin <woals99@gmail.com>
2022-02-02 03:08:25 +0900

    [진행중] 맞춤법 크롤링

--- src/pages/Default.tsx
+++ src/pages/Default.tsx
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 import Header from './Header';
 import '../css/main.css';
+import axios from 'axios';
+import { Cheerio } from 'cheerio';
 
 function Default() {
   
   const [major, setMajor] = useState('');
   const [name, setName] = useState('');
   const [firstSelect, setFirstSelect] = useState('');
+  const [showChecker, setShowChecker] = useState(false);
   let copiedForm: string = '';
+  let naverCheckerURL: string = 'https://m.search.naver.com/p/csearch/ocontent/util/SpellerProxy?_callback=jQuery1124029748229850476093_1643737965873&q=%EB%82%98%EB%8A%94+%ED%95%99%EC%83%9D+%EC%9E%85%EB%8B%88%EB%8B%A4.&where=nexearch&color_blindness=0&_=1643737965875';
 
   function copyInClipboard(){
     //console.log(copiedForm);
@@ -52,8 +56,10 @@ function Default() {
     <div className='buttonContainer'>
       <div onClick={copyBtnClickHandler}>복사하기</div>
       <div onClick={()=>{window.location.replace("/")}}>clear</div>
-      <div>맞춤법 검사</div>
+      <div onClick={()=>{setShowChecker(!showChecker)}}>맞춤법 검사</div>
     </div>
+
+    <textarea className={showChecker==true?'showTextArea':'hideTextArea'}></textarea>
   </div>);
 }
 

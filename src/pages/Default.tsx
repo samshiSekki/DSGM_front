1da087ef05731daf426fe0d1bf642aef9de77a66
xongjaemin <woals99@gmail.com>
2022-02-03 01:33:13 +0900

    맞춤법 검사 거의 다 만듬

--- src/pages/Default.tsx
+++ src/pages/Default.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import Header from './Header';
-import '../css/main.css';
 import axios from 'axios';
-import { Cheerio } from 'cheerio';
+import '../css/main.css';
+import { json } from 'stream/consumers';
 
 function Default() {
   
@@ -10,8 +10,33 @@ function Default() {
   const [name, setName] = useState('');
   const [firstSelect, setFirstSelect] = useState('');
   const [showChecker, setShowChecker] = useState(false);
+  let checkerResultDataString: string;
+  //let checkerResultDataJson: JSON;
   let copiedForm: string = '';
-  let naverCheckerURL: string = 'https://m.search.naver.com/p/csearch/ocontent/util/SpellerProxy?_callback=jQuery1124029748229850476093_1643737965873&q=%EB%82%98%EB%8A%94+%ED%95%99%EC%83%9D+%EC%9E%85%EB%8B%88%EB%8B%A4.&where=nexearch&color_blindness=0&_=1643737965875';
+  let naverCheckerURL: string;
+
+  const getChecker = async() => {
+    naverCheckerURL = 'https://m.search.naver.com/p/csearch/ocontent/util/SpellerProxy?_callback=mycallback&q=' + '교수님안녕하새요!' + '&where=nexearch&color_blindness=0&_=1643811632694';
+    let checkResult : any = axios.get(naverCheckerURL);
+    let getData = ()=>{
+      checkResult.then((appData : any)=>{
+        checkerResultDataString = appData.data;
+        //console.log(checkerResultData);
+        checkerResultDataString = checkerResultDataString.replace('mycallback(','').replace(');', '');
+        //console.log(checkerResultDataString);
+        //checkerResultDataJson = JSON.parse(checkerResultDataString);
+        //console.log(JSON.parse(checkerResultDataString).message.result.notag_html);
+        checkerResultDataString = JSON.parse(checkerResultDataString).message.result.notag_html;
+        console.log(checkerResultDataString);
+      });
+    };
+    getData();
+  }
+
+  function checkerBtnClickHandler(){
+    setShowChecker(!showChecker);
+    getChecker();
+  }
 
   function copyInClipboard(){
     //console.log(copiedForm);
@@ -56,7 +81,7 @@ function Default() {
     <div className='buttonContainer'>
       <div onClick={copyBtnClickHandler}>복사하기</div>
       <div onClick={()=>{window.location.replace("/")}}>clear</div>
-      <div onClick={()=>{setShowChecker(!showChecker)}}>맞춤법 검사</div>
+      <div onClick={checkerBtnClickHandler}>맞춤법 검사</div>
     </div>
 
     <textarea className={showChecker==true?'showTextArea':'hideTextArea'}></textarea>

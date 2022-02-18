ec0329996c8acf8e15309fb7aa5b7fa0a5466813
xongjaemin <woals99@gmail.com>
2022-02-19 02:57:16 +0900

    feat: recommend, grade copy

--- src/components/Post/Grade/GradeContent.tsx
+++ src/components/Post/Grade/GradeContent.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
+import {connect, useDispatch} from 'react-redux';
 
 type ContentProps = {
     content : string
@@ -20,6 +21,8 @@ const GradeContent = ({num,setNum}:NumberProps) => {
     const textRef2 = useRef<any>(null);
     const textRef3 = useRef<any>(null);
     const textRef4 = useRef<any>(null);
+
+    let dispatch = useDispatch();
 
     const handleResize = useCallback(
         (value:number) => {
@@ -72,40 +75,46 @@ const GradeContent = ({num,setNum}:NumberProps) => {
                 setCurrent(response.data[num]);
                 console.log(response.data[num]);
             }) */
-
+        
+        dispatch({type: 'change', payload:{changeData: num, variableType: 'recommendState'}});
     },[num]);
+
+    function changeInputValue(e: any, variableType: string){
+        dispatch({type: 'change', payload:{changeData: e.target.value, variableType: variableType}});
+    }
+
     return (
         <>
         <div>
         {num == 0?
             <>
-             <div>다름이 아니라, 이번 <InputDiv></InputDiv>수업에서 <InputDiv></InputDiv>부분에 대해 저는 최선을 다했습니다. </div>
+             <div>다름이 아니라, 이번 <InputDiv onChange={(e)=>changeInputValue(e, 'gradeContent0_1')}></InputDiv>수업에서 <InputDiv onChange={(e)=>changeInputValue(e, 'gradeContent0_2')}></InputDiv>부분에 대해 저는 최선을 다했습니다. </div>
              {state1 == false ? <ButtonStyled onClick={()=>setState1(true)}>+</ButtonStyled> :
-             <><ButtonStyled onClick={()=>setState1(false)}>-</ButtonStyled> <TextArea ref={textRef1} onInput={()=>handleResize(1)}></TextArea></>}
+             <><ButtonStyled onClick={()=>setState1(false)}>-</ButtonStyled> <TextArea ref={textRef1} onInput={()=>handleResize(1)} onChange={(e)=>changeInputValue(e, 'gradeContent0_plus1')}></TextArea></>}
             <div>그럼에도 예상과 달리 다소 낮은 성적을 받아 조금 아쉽습니다. </div>
             {state2 == false ? <ButtonStyled onClick={()=>setState2(true)}>+</ButtonStyled> :
-             <><ButtonStyled onClick={()=>setState2(false)}>-</ButtonStyled> <TextArea ref={textRef2} onInput={()=>handleResize(2)}></TextArea></>}
+             <><ButtonStyled onClick={()=>setState2(false)}>-</ButtonStyled> <TextArea ref={textRef2} onInput={()=>handleResize(2)} onChange={(e)=>changeInputValue(e, 'gradeContent0_plus2')}></TextArea></>}
             <div>어떤 부분이 부족하여 이런 성적을 받았는지 여쭙고 싶어 메일을 작성하게 되었습니다. </div>
             {state3 == false ? <ButtonStyled onClick={()=>setState3(true)}>+</ButtonStyled> : 
-             <><ButtonStyled onClick={()=>setState3(false)}>-</ButtonStyled><TextArea ref={textRef3} onInput={()=>handleResize(3)}></TextArea></>}
+             <><ButtonStyled onClick={()=>setState3(false)}>-</ButtonStyled><TextArea ref={textRef3} onInput={()=>handleResize(3)} onChange={(e)=>changeInputValue(e, 'gradeContent0_plus3')}></TextArea></>}
             <div>교수님의 피드백을 바탕으로 이후 부족한 점을 보완하고자 하니, 바쁘시겠지만 꼭 한 번 다시 검토해주시면 감사하겠습니다.</div>
             {state4 == false ? <ButtonStyled onClick={()=>setState4(true)}>+</ButtonStyled> : 
-             <><ButtonStyled onClick={()=>setState4(false)}>-</ButtonStyled><TextArea ref={textRef4} onInput={()=>handleResize(4)}></TextArea></>}
+             <><ButtonStyled onClick={()=>setState4(false)}>-</ButtonStyled><TextArea ref={textRef4} onInput={()=>handleResize(4)} onChange={(e)=>changeInputValue(e, 'gradeContent0_plus4')}></TextArea></>}
             </>
             :
             <>
              <div>다름이 아니라 각 항목에 대한 취득 점수 및 상세 성적을 알 수 있을까 싶어 메일을 보내게 되었습니다. </div>
              {state1 == false ? <ButtonStyled onClick={()=>setState1(true)}>+</ButtonStyled> :
-             <><ButtonStyled onClick={()=>setState1(false)}>-</ButtonStyled> <TextArea ref={textRef1} onInput={()=>handleResize(1)}></TextArea></>}
+             <><ButtonStyled onClick={()=>setState1(false)}>-</ButtonStyled> <TextArea ref={textRef1} onInput={()=>handleResize(1)} onChange={(e)=>changeInputValue(e, 'gradeContent1_plus1')}></TextArea></>}
             <div>과제와 시험 모두 나름대로 열심히 준비했는데, 예상한 결과보다는 아쉬운 결과에 문의를 드리게 되었습니다.  </div>
             {state2 == false ? <ButtonStyled onClick={()=>setState2(true)}>+</ButtonStyled> :
-             <><ButtonStyled onClick={()=>setState2(false)}>-</ButtonStyled> <TextArea ref={textRef2} onInput={()=>handleResize(2)}></TextArea></>}
+             <><ButtonStyled onClick={()=>setState2(false)}>-</ButtonStyled> <TextArea ref={textRef2} onInput={()=>handleResize(2)} onChange={(e)=>changeInputValue(e, 'gradeContent1_plus2')}></TextArea></>}
             <div>제가 어떤 부분에서 부족했고, 앞으로 어떠한 부분을 보완하는 것이 좋을지 의견을 여쭙고 싶어 실례를 무릅쓰고 메일 드립니다.</div>
             {state3 == false ? <ButtonStyled onClick={()=>setState3(true)}>+</ButtonStyled> :
-             <><ButtonStyled onClick={()=>setState3(false)}>-</ButtonStyled> <TextArea ref={textRef3} onInput={()=>handleResize(3)}></TextArea></>}
+             <><ButtonStyled onClick={()=>setState3(false)}>-</ButtonStyled> <TextArea ref={textRef3} onInput={()=>handleResize(3)} onChange={(e)=>changeInputValue(e, 'gradeContent1_plus3')}></TextArea></>}
             <div>제가 어떤 부분이 부족했는지 알려주시면 학업에 참고하여 보완하고 싶습니다.</div>
             {state4 == false ? <ButtonStyled onClick={()=>setState4(true)}>+</ButtonStyled> :
-             <><ButtonStyled onClick={()=>setState4(false)}>-</ButtonStyled> <TextArea ref={textRef4} onInput={()=>handleResize(4)}></TextArea></>}
+             <><ButtonStyled onClick={()=>setState4(false)}>-</ButtonStyled> <TextArea ref={textRef4} onInput={()=>handleResize(4)} onChange={(e)=>changeInputValue(e, 'gradeContent1_plus4')}></TextArea></>}
             </>}
            
         </div>
@@ -115,7 +124,12 @@ const GradeContent = ({num,setNum}:NumberProps) => {
 
 };
 
-export default GradeContent;
+function f1(inputValue: any){
+    return {
+      inputValue : inputValue
+    }
+}
+export default connect(f1)(GradeContent);
 
 const InputDiv = styled.input`
 font-weight:bold;

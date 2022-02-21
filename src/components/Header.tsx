b0a70100b36996eec145ad0e7c27fedaa00f141a
xongjaemin <woals99@gmail.com>
2022-02-22 01:25:36 +0900

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -53,33 +53,6 @@ function Header() {
       <div className='title'>
         <img src="img/dsgm_title.png" className='dsgmTitleImg'/>
       </div>
-      {/*
-      <div className='tabmenuContainer'>
-        <Link to='/'>
-          <button className='tabButton'>기본</button>
-        </Link>
-        <Link to='/billnut'>
-          <button className='tabButton'>빌넣</button>
-        </Link>
-        <Link to='recommend'>
-        <button className='tabButton'>추천서</button>
-        </Link> 
-        <Link to='grade'>
-        <button className='tabButton'>성적문의</button>
-        </Link>
-        <div className='dropdown'>
-          <button className='dropButton'>기타</button>
-          <div className = 'dropdown-content'>
-            <a>대학원 진학 문의</a>
-            <a>조교 신청 문의</a>
-            <a>진로 상담</a>
-            <a>수업 내용 질문</a>
-            <a>과제 질문</a>
-          </div>
-        </div>
-        
-      </div>
-      */}
       <CurrentIndicator src="img/Group 8.png"/>
       <TabWrap>
       <MenuTabBar>

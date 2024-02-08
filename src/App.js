b8ebc685bd01a78701d5235b897f4653bfb20a73
xongjaemin <woals99@gmail.com>
2024-02-09 02:59:53 +0900

    refactor: 미사용 컴포넌트 파일 삭제

--- src/App.js
+++ src/App.js
@@ -1,41 +1,38 @@
-import Header from './components/Header';
-import Footer from 'components/Footer';
-import Default from './pages/Default';
-import Billnut from './pages/Billnut';
-import Recommend from './pages/Recommend';
-import Grade from './pages/Grade';
-import Others from 'pages/Others';
-import { Route, Routes, useLocation } from 'react-router-dom';
-import { useEffect } from 'react';
-import ReactGA from 'react-ga';
-import GraduateSchool from 'pages/Others/GraduateSchool';
-import AttSchool from 'pages/Others/AttSchool';
-import AttPersonal from 'pages/Others/AttPersonal';
-import Question from 'pages/Others/Question';
+import Header from "./components/Header";
+import Footer from "components/Footer";
+import Default from "./pages/Default";
+import Billnut from "./pages/Billnut";
+import Recommend from "./pages/Recommend";
+import Grade from "./pages/Grade";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import ReactGA from "react-ga";
+import GraduateSchool from "pages/Others/GraduateSchool";
+import AttSchool from "pages/Others/AttSchool";
+import AttPersonal from "pages/Others/AttPersonal";
+import Question from "pages/Others/Question";
 
 ReactGA.event({
-  category: 'User',
-  action: 'Created an Account'
+  category: "User",
+  action: "Created an Account",
 });
 ReactGA.exception({
-  description: 'An error ocurred',
-  fatal: true
+  description: "An error ocurred",
+  fatal: true,
 });
 
 function App() {
   const location = useLocation();
-  
-  useEffect(()=>{
-    ReactGA.initialize("user id");
-  },[]);
 
-  
+  useEffect(() => {
+    ReactGA.initialize("user id");
+  }, []);
+
   useEffect(() => {
     ReactGA.set({ page: location.pathname }); // Update the user's current page
     ReactGA.pageview(location.pathname); // Record a pageview for the given page
   }, [location]);
 
-  
   return (
     <div className="App">
       <Routes>
@@ -48,7 +45,7 @@ function App() {
         <Route path="/attendancepersonal" element={<AttPersonal />} />
         <Route path="/questions" element={<Question />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
